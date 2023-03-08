629c2072d0488da98a4937c57ac5592618c68330
revathymelayi <rmelayi12@gmail.com>
2023-03-08 12:15:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Todo from "./Todoapp/Todo";
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      < Todo />
+    </div>
+  );
+}
+
+export default App;
